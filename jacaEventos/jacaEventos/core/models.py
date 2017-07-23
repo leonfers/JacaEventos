1fa28487ed5c2d1ff0bd763f27db5d9198f9bc1a
leaojn <arealeaoneto@gmail.com>
2017-07-23 15:55:47 -0300

    Correcoes

--- jacaEventos/jacaEventos/core/models.py
+++ jacaEventos/jacaEventos/core/models.py
@@ -1,50 +1,35 @@
 from django.db import models
-from jacaEventos.utils.models import Periodo
-
-
+from jacaEventos.utils import EscolhaEnum
 
 # Create your models here.
 class Atividade(models.Model):
 
     descricao = models.TextField('Descricao da atividade', blank=True)
-    periodo = models.OneToOneField(
-        Periodo,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
     valor_da_atividade = models.DecimalField("Valor", max_digits=5, decimal_places=2)
-    atividades_diponiveis = models.ForeignKey('usuario.Inscricao', related_name='atividades_disponiveis', blank=True, null=True)
-    atividades_evento = models.ForeignKey('core.Evento',verbose_name="Atividades", related_name="Atividades_do_evento")
+    tipo_atividade = models.CharField(max_length=1, choices=EscolhaEnum.choices())
 
 
-class Evento(models.Model):
+# Classes de Enum referente ao core
+# info:
+# para receber o enum na classes adicionar a seguinte linha:
+# <Varivel_tipo> = models.CharField(max_length=1, choices=EscolhaEnum.choices())
 
-    descricao = models.TextField('Descricao do evento', blank=True)
-    periodo = models.OneToOneField(
-        Periodo,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
-    meus_eventos = models.ForeignKey('usuario.Usuario', related_name='meus_eventos', blank=True, null=True)
-    administrador_evento = models.ForeignKey('usuario.Usuario',verbose_name="Administrador")
+class StatusEvento(EscolhaEnum):
+    inscricoes_abertas = 0
+    incricoes_fechado = 1
+    encerrado = 2
+    andamento = 4
 
-class Instituicao(models.Model):
-
-    nome = models.CharField('Nome da instituiçao', max_length=30)
-    tipo_de_relacionamento = models.TextField('tipo_de_relacionamento', blank=True)
-    instituicao_relacionada = models.ForeignKey(Evento,verbose_name="Instituicoes_")
-
-
-class Evento_Instituicao(models.Model):
-
-    tipo_relacionamento = models.TextField('Tipo Relacionamento', blank=True)
-    instituicao = models.ForeignKey(Instituicao)
-    instituicao_relacionada = models.ForeignKey(Evento,verbose_name="Instituicoes", related_name="Instituicoes_relacionadas")
-
-class Tag(models.Model):
-
-    nome_tag = models.CharField('Tag', max_length=30)
-    tag_evento = models.ForeignKey('core.Evento',verbose_name="tags_evento", related_name="tags_evento")
-    tag_interesse = models.ForeignKey('usuario.Usuario',verbose_name="tags_interesse", related_name="tags_usuario")
+class TipoAtividade(EscolhaEnum):
+    palestra = 0
+    minicurso = 1
+    workshop = 2
+    mesa_redonda = 3
 
 
+class TipoEvento(EscolhaEnum):
+    congresso = 0
+    semana = 1
+    seminario = 2
+
+###
