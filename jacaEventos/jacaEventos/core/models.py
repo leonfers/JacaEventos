5b9007232efc0714ded160537a933ba167fb04a2
leaojn <arealeaoneto@gmail.com>
2017-07-23 15:36:08 -0300

    Adicionado mais modelos

--- jacaEventos/jacaEventos/core/models.py
+++ jacaEventos/jacaEventos/core/models.py
@@ -7,8 +7,14 @@ from jacaEventos.utils.models import Periodo
 class Atividade(models.Model):
 
     descricao = models.TextField('Descricao da atividade', blank=True)
+    periodo = models.OneToOneField(
+        Periodo,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     valor_da_atividade = models.DecimalField("Valor", max_digits=5, decimal_places=2)
     atividades_diponiveis = models.ForeignKey('usuario.Inscricao', related_name='atividades_disponiveis', blank=True, null=True)
+    atividades_evento = models.ForeignKey('core.Evento',verbose_name="Atividades", related_name="Atividades_do_evento")
 
 
 class Evento(models.Model):
@@ -24,7 +30,8 @@ class Evento(models.Model):
 
 class Instituicao(models.Model):
 
-    tipo_de_relacionamento = models.TextField('Nome da instituicao', blank=True)
+    nome = models.CharField('Nome da instituiçao', max_length=30)
+    tipo_de_relacionamento = models.TextField('tipo_de_relacionamento', blank=True)
     instituicao_relacionada = models.ForeignKey(Evento,verbose_name="Instituicoes_")
 
 
@@ -33,5 +40,11 @@ class Evento_Instituicao(models.Model):
     tipo_relacionamento = models.TextField('Tipo Relacionamento', blank=True)
     instituicao = models.ForeignKey(Instituicao)
     instituicao_relacionada = models.ForeignKey(Evento,verbose_name="Instituicoes", related_name="Instituicoes_relacionadas")
+
+class Tag(models.Model):
+
+    nome_tag = models.CharField('Tag', max_length=30)
+    tag_evento = models.ForeignKey('core.Evento',verbose_name="tags_evento", related_name="tags_evento")
+    tag_interesse = models.ForeignKey('usuario.Usuario',verbose_name="tags_interesse", related_name="tags_usuario")
 
 
