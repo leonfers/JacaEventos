8d5927e0f2661167ff2559f9435539a090426d36
KassioHolanda <holandakassio@hotmail.com>
2017-07-27 20:58:18 -0300

    atualizando telas login e cadastro

--- jacaEventos/jacaEventos/usuario/urls.py
+++ jacaEventos/jacaEventos/usuario/urls.py
@@ -8,4 +8,5 @@ from jacaEventos.usuario import views
 urlpatterns = [
     url('^registrar/$', views.registrar_usuario, name='registrar_usuario'),
     url(r'^entrar/', login, {'template_name': 'login_usuario.html'}, name='login_usuario'),
+
 ]
