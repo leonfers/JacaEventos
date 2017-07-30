0b2c66d6ff3cbf4300f460fcb9584ae6b6104bc0
KassioHolanda <holandakassio@hotmail.com>
2017-07-30 13:20:06 -0300

    organizando telas de login

--- jacaEventos/jacaEventos/usuario/urls.py
+++ jacaEventos/jacaEventos/usuario/urls.py
@@ -6,8 +6,9 @@ from django.contrib.auth.views import login, logout, logout_then_login
 from jacaEventos.usuario import views
 
 urlpatterns = [
-    url('^registrar/$', views.registrar_usuario, name='registrar_usuario'),
-    url('^entrar/', login, {'template_name': 'login_usuario.html'}, name='login_usuario'),
+    url('^registrar/$', views.registrar, name='registrar'),
+    url('^pagina_inicial/$', views.pagina_inicial, name='pagina_inicial'),
+    url('^$', login, {'template_name': 'login.html'}, name='login'),
     # url('^', login, {'template_name': 'login_usuario.html'}, name='login_usuario'),
-    # url('^sair/', logout_then_login, {'login_url' : '/login_usuario'}, name='logout_usuario')
+    url('^logout/', logout_then_login, {'login_url' : 'login'}, name='logout_usuario')
 ]
