3c8499a89b9a29902178e44ad1d1fe4a17b0df61
leaojn <arealeaoneto@gmail.com>
2017-08-01 23:34:45 -0300

    heroku

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 from decouple import config
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE"))
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'jacaEventos.settings.producao')
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
