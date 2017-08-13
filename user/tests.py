1e36e66e850ca3f6726885afc77cd76bc1f761b3
Akijunior <suitsu19@gmail.com>
2017-08-13 13:38:04 -0300

    Adaptação e adição de alguns testes referentes a pagamento e usuário.

--- user/tests.py
+++ user/tests.py
@@ -11,18 +11,17 @@ from core.models import *
 class TestesPerfilUsuario(TestCase):
 
 
-    def func(x):
-        return x + 1
-
-    def test_answer():
-        assert func(3) == 5
-
-    def Iniciar_Usuario(self):
-        Usuario.objects.create_user(email="teste@teste", senha="testando")
+    def test_iniciar_Usuario_com_email_e_senha(self):
+        usuario = Usuario(email="teste@teste")
+        self.assertFalse(Usuario.email, None)
     
-    def Criacao_Lista_Vazia_de_Evento_Para_Usuario_recem_criado(self):
+    def test_criacao_Lista_Vazia_de_Evento_para_Usuario_recem_criado(self):
         usuario = Usuario()
-        self.assertTrue(usuario.get_eventos(self), [])
+        self.assertEqual(usuario.get_eventos(), [])
+    
+    def test_criacao_Lista_Vazia_de_Inscricoes_para_Usuario_recem_criado(self):
+        usuario = Usuario()
+        self.assertEqual(usuario.get_inscricoes(), [])
     
     def test_nao_permitir_criacao_de_Evento_sem_nome(self):
         
@@ -31,7 +30,7 @@ class TestesPerfilUsuario(TestCase):
 
     def test_email_nao_nulo(self):
         usuario = Usuario(email = '')
-        self.assertIs(usuario.get_email_field_name, False)
+        self.assertFalse(usuario.email, '')
 
     def test_username_em_branco(self):
          usuario = Usuario(username = '')
@@ -41,18 +40,23 @@ class TestesPerfilUsuario(TestCase):
 class TesteInscricao(TestCase):
 
 
-    def Calcular_valor_correto_de_inscricao_dado_um_conjunto_de_atividades_adicionadas(self):
+    def test_calcular_valor_correto_de_inscricao_dado_um_conjunto_de_atividades_adicionadas(self):  
+        """
+        Atualiza o valor da inscricao conforme a adicao de novas atividades ao pacote do usuario.
+        """
+        atividade = Atividade(valor=5.0)
+        evento_criado = Evento()
+        evento_criado.add_atividade(self, atividade)
+        inscricao = Inscricao(evento = evento_criado)
         
-    """
-    Atualiza o valor da inscricao conforme a adicao de novas atividades ao pacote do usuario.
-    """
-    atividade = Atividade(valor=5.0)
-    evento_criado = Evento()
-    evento_criado.add_atividade(self, atividade)
-    inscricao = Inscricao(evento = evento_criado)
+        self.assertEqual(evento_criado.valor, inscricao.evento.valor)
     
-    self.assertEqual(evento_criado.valor, inscricao.evento.valor)
-        
+    def test_nao_aceitar_incluir_inscricao_se_o_estado_ja_for_ATIVO(self):
+        """
+        Verifica o status_inscricao comparando se e ativa, e se for retorna erro.
+        """  
+        inscricao = Inscricao(status_inscricao=ATIVA)
+        self.assertEqual(inscricao.status_inscricao, ATIVA)
     
 
     
