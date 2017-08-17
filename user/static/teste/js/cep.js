be73dc767b8a2d0dfed30ae0d2f413a3a00887aa
leaojn <arealeaoneto@gmail.com>
2017-08-17 11:22:57 -0300

    organizando js

---
+++ user/static/teste/js/cep.js
@@ -0,0 +1,18 @@
+$("input[name=cep]").blur(function() {
+    var cep = $(this).val().replace(/[^0-9]/, '');
+    var url = 'https://correiosapi.apphb.com/cep/'+cep;
+    $.ajax({
+        url: url,
+        type: "GET",
+        dataType: "jsonp",
+        success: function (data) {
+        $("input[name=pais]").val("Brasil");
+        $("input[name=estado]").val(data.estado);
+        $("input[name=cidade]").val(data.cidade);
+        $("input[name=logradouro]").val(data.tipoDeLogradouro + " " + data.logradouro);
+
+
+    }
+    });
+
+});
