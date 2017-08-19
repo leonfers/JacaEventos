2ee1ab70b558c7dbb448a8121c029e40f728b6aa
KassioHolanda <holandakassio@hotmail.com>
2017-08-19 14:58:16 -0300

    correcoes nos templates

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url('^meus_eventos/(?P<eventos_id>\d+)$', views.exibir_evento, name='exibir_evento'),
     url('^registrar_evento/$', views.registrar_eventos, name='registrar_eventos'),
     url('^registrar_instituicoes/$', views.registrar_instituicoes, name='registrar_instituicoes'),
+    url('^participar_evento/$', views.participar_evento, name='participar_evento'),
 ]
