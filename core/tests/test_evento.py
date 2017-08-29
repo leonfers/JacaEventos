bf73a5d33000ac7ab32f059675b95950f1254388
Akijunior <suitsu19@gmail.com>
2017-08-28 22:15:12 -0300

    Atualizando dos testes para ficarem com maior concordância com o V3.

--- core/tests/test_evento.py
+++ core/tests/test_evento.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.test import TestCase
 from django.urls import reverse
 from utils.models import *
+from user.models import *
 from core.models import *
 
 
@@ -21,7 +22,7 @@ class TesteEvento(TestCase):
         self.assertEqual(evento.endereco, endereco_evento)
         self.assertEqual(evento.dono.nome, 'Ele')
         self.assertEqual(evento.nome, 'Evento inicial')
-        self.assertEqual(evento.tipo_evento, 'padrao', "Todos os testes passaram com êxito!")
+        self.assertEqual(evento.tipo_evento, 'padrao')
     
     def test_evento_criado_com_dono_e_conjunto_vazio_de_atividades(self):
         o_dono = Usuario(nome='Ele')
@@ -41,3 +42,37 @@ class TesteEvento(TestCase):
         atividade_repetida = Atividade(nome="Teste")
         evento = Evento()
         self.assertFalse(atividade_repetida in evento.get_atividades())
+    
+    def test_mudanca_de_comportamentos_que_sejam_observados_por_outros_componentes(self):
+        o_dono = Usuario(nome='Ele')
+        endereco_evento = Endereco(pais='Brasil', cidade='Ipanema', bairro='Dirceu', logradouro='Rua das avenidas',
+                                   numero='41', cep='44522-98', estado='Piaui')
+        periodo_evento = Periodo(data_inicio='2017-08-14', data_fim='2017-08-18')
+        evento = Evento(dono=o_dono, nome="Evento inicial", tipo_evento='padrao',
+                        endereco=endereco_evento, periodo=periodo_evento)
+        evento_novo = evento.periodo.data_inicio='2017-08-10'
+        self.assertFalse(evento != evento_novo, "Informações de evento foram modificadas.")
+
+    def test_apenas_evento_satelite_pode_ser_adicionado_sozinho_a_inscricao(self):
+        """
+        Em caso de Eventos Satélites será possível se inscrever isoladamente em
+        eventos que são satélites de eventos principais, porém não será permitido se
+        inscrever apenas no evento Principal.
+        """
+        evento_novo = Evento(nome='evento')
+        evento_extra = Evento(nome='evento grande')
+        evento_satelite = EventoSatelite(eventos=evento_extra)
+        usuario = Usuario(nome='Usuario')
+        atividade = Atividade(nome='Atividade')
+        trilha = Trilha(id=1, nome = 'trilha', valor = 15.00, evento = evento_novo)
+        trilhaSatelite = Trilha(id=2, nome='trilha', valor=15.00, evento=evento_extra)
+        inscricao = Inscricao(id=1, status_inscricao = 'ativa', usuario = usuario, evento = evento_novo)
+        self.assertFalse(TrilhaInscricao(trilha, inscricao))
+        self.assertTrue(TrilhaInscricao(trilhaSatelite, inscricao))
+
+
+
+
+
+
+   
--- user/tests/test_inscricao.py
+++ user/tests/test_inscricao.py
@@ -22,3 +22,15 @@ class TesteInscricao(TestCase):
         inscricao = Inscricao(status_inscricao='ativa')
         self.assertFalse(inscricao.evento, evento)
     
+    def test_calculo_do_valor_final_de_uma_inscricao_e_o_somatorio_dos_itens_para_os_eventos_onde_a_inscricao_e_por_itens(self):
+        """
+        O Calculo do valor final de um inscrição é o somatório dos itens para os eventos
+        onde a inscrição é por itens.
+        """
+    
+    def test_calculo_do_valor_final_de_uma_inscricao_direta_deve_ser_especificado_no_proprio_evento(self):
+        """
+        Já para eventos de inscrição direto no evento o valor
+        será especificado direto no evento, incluindo os valores do eventos Satelites.
+        """
+    
