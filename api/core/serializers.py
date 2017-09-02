82f1042d667451b2f56165e594fd25a773ee4ca3
leaojn <arealeaoneto@gmail.com>
2017-09-02 16:43:01 -0300

    correcoes basicas

--- api/core/serializers.py
+++ api/core/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
-from core.models import Evento,Atividade
+from core.models import Evento, Atividade
+
 
 class EventosSerializer(serializers.ModelSerializer):
+    dono = serializers.CharField(source='get_dono', read_only=False)
+   # tipo = serializers.CharField(source='get_tipo', read_only=True)
 
-    dono = serializers.CharField(source='get_dono',read_only=True)
-    tipo = serializers.CharField(source='get_tipo',read_only=True)
     # valor = serializers.DecimalField(source='get_valor',read_only=True, max_digits=5, decimal_places=2,)
 
     class Meta:
         model = Evento
-        fields = ('id','nome','dono','valor','tipo')
+        fields = ('id', 'nome', 'descricao','dono' ,'valor', 'periodo', 'endereco')
+
 
 class AtividadeSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Atividade
-        fields = ('id','nome','valor','evento','periodo')
+        fields = ('id', 'nome', 'valor', 'evento', 'periodo', 'horario')
--- core/helpers.py
+++ core/helpers.py
@@ -1,45 +1,50 @@
 # METODOS DO FORMULARIO
 import pycep_correios
 from pycep_correios import CEPInvalido
-
+from django.shortcuts import redirect
+from django.conf import settings
 from core.forms import RegistrarTagEventosForm, RegistrarGerentesForm, RegistrarEspacoFisicoEventoForm, \
     AssociarInstituicoesEventoForm, AdicionarEventosSateliteForm, RegistrarAtividadeContinuaForm, \
     RegistrarAtividadeAdministrativaForm, RegistrarAtividadeForm, TrilhaAtividadeEventoForm
 from utils.forms import PeriodoForm, HorarioForm
+from django.http import HttpResponseRedirect
+
 
 # formulario para registro de tags
-def formulario_tag( evento, request ):
-    form_tag_evento = RegistrarTagEventosForm( request.POST )
+def formulario_tag(evento, request):
+    form_tag_evento = RegistrarTagEventosForm(request.POST)
 
     if form_tag_evento.is_valid():
-        tag = form_tag_evento.save( commit=False )
+        tag = form_tag_evento.save(commit=False)
         tag.save()
         # metodo para adicionar tag ao evento
-        evento.add_tag( tag )
+        evento.add_tag(tag)
         form_tag_evento = RegistrarTagEventosForm()
 
+
 # formulario para registro de gerentes
-def formulario_gerente( evento, request ):
-    form_gerentes = RegistrarGerentesForm( request.POST )
+def formulario_gerente(evento, request):
+    form_gerentes = RegistrarGerentesForm(request.POST)
 
     if form_gerentes.is_valid():
-        gerente = form_gerentes.save( commit=False )
+        gerente = form_gerentes.save(commit=False)
         gerente.evento = evento
         gerente.save()
         # form_gerentes = RegistrarGerentes()
 
+
 # formulario para registro de atividade padrao
-def formulario_atividade_padrao( form_horario, evento, request ):
-    form_periodo = PeriodoForm( request.POST )
-    form_atividade_padrao = RegistrarAtividadeForm( request.POST )
+def formulario_atividade_padrao(form_horario, evento, request):
+    form_periodo = PeriodoForm(request.POST)
+    form_atividade_padrao = RegistrarAtividadeForm(request.POST)
 
     if form_atividade_padrao.is_valid() and form_horario.is_valid():
-        atividade_padrao = form_atividade_padrao.save( commit=False )
+        atividade_padrao = form_atividade_padrao.save(commit=False)
         # formulario periodo da atividade
-        periodo = form_periodo.save( commit=False )
+        periodo = form_periodo.save(commit=False)
         periodo.save()
         # formulario horario atividade
-        horario = form_horario.save( commit=False )
+        horario = form_horario.save(commit=False)
         horario.save()
         # adicionando horario e periodo ao registro de atividade
         atividade_padrao.horario = horario
@@ -47,20 +52,21 @@ def formulario_atividade_padrao( form_horario, evento, request ):
         atividade_padrao.periodo = periodo
         atividade_padrao.save()
         # adicionando atividade registrada ao registro de eventos
-        evento.add_atividade( atividade_padrao )
+        evento.add_atividade(atividade_padrao)
+
 
 # formulario para registro de atividade administrativa
-def formulario_atividade_administrativa( form_horario, evento, request ):
+def formulario_atividade_administrativa(form_horario, evento, request):
     form_periodo = PeriodoForm(request.POST)
-    form_atividade_administrativa = RegistrarAtividadeAdministrativaForm( request.POST )
+    form_atividade_administrativa = RegistrarAtividadeAdministrativaForm(request.POST)
 
     if form_atividade_administrativa.is_valid() and form_horario.is_valid():
-        atividade_administrativa = form_atividade_administrativa.save( commit=False )
+        atividade_administrativa = form_atividade_administrativa.save(commit=False)
         # formulario horario atividade
-        horario = form_horario.save( commit=False )
+        horario = form_horario.save(commit=False)
         horario.save()
         # formulario periodo da atividade
-        periodo = form_periodo.save( commit=False )
+        periodo = form_periodo.save(commit=False)
         periodo.save()
         # adicionando horario e periodo ao registro de atividade
         atividade_administrativa.horario = horario
@@ -68,20 +74,21 @@ def formulario_atividade_administrativa( form_horario, evento, request ):
         atividade_administrativa.periodo = periodo
         atividade_administrativa.save()
         # adicionando atividade registrada ao registro de eventos
-        evento.add_atividade( atividade_administrativa )
+        evento.add_atividade(atividade_administrativa)
+
 
 # formulario para registro de atividade continua
-def formulario_atividade_continua( form_horario, evento, request ):
-    form_periodo = PeriodoForm( request.POST )
-    form_atividade_continua = RegistrarAtividadeContinuaForm( request.POST )
+def formulario_atividade_continua(form_horario, evento, request):
+    form_periodo = PeriodoForm(request.POST)
+    form_atividade_continua = RegistrarAtividadeContinuaForm(request.POST)
 
     if form_atividade_continua.is_valid() and form_horario.is_valid():
-        atividade_continuna = form_atividade_continua.save( commit=False )
+        atividade_continuna = form_atividade_continua.save(commit=False)
         # formulario horario atividade
-        horario = form_horario.save( commit=False )
+        horario = form_horario.save(commit=False)
         horario.save()
         # formulario periodo da atividade
-        periodo = form_periodo.save( commit=False )
+        periodo = form_periodo.save(commit=False)
         periodo.save()
         # adicionando atividade registrada ao registro de eventos
         atividade_continuna.horario = horario
@@ -89,34 +96,38 @@ def formulario_atividade_continua( form_horario, evento, request ):
         atividade_continuna.periodo = periodo
         atividade_continuna.save()
         # adicionando atividade registrada ao registro de evento
-        evento.add_atividade( atividade_continuna )
+        evento.add_atividade(atividade_continuna)
+
 
 # formulario para registro de eventos satelite
-def formulario_evento_satelite( evento, request ):
-    form_evento_satelite = AdicionarEventosSateliteForm( request.POST )
+def formulario_evento_satelite(evento, request):
+    form_evento_satelite = AdicionarEventosSateliteForm(request.POST)
 
     if form_evento_satelite.is_valid():
-        evento_satelite = form_evento_satelite.save( commit=False )
+        evento_satelite = form_evento_satelite.save(commit=False)
         evento_satelite.eventos = evento
         evento_satelite.save()
 
+
 # formulario para registro de instituicoes relacionadas ao evento
-def formulario_intituicao_evento( evento, request ):
-    form_instituicao_evento = AssociarInstituicoesEventoForm( request.POST )
+def formulario_intituicao_evento(evento, request):
+    form_instituicao_evento = AssociarInstituicoesEventoForm(request.POST)
 
     if form_instituicao_evento.is_valid():
-        instituicao_evento = form_instituicao_evento.save( commit=False )
+        instituicao_evento = form_instituicao_evento.save(commit=False)
         instituicao_evento.evento_relacionado = evento
         instituicao_evento.save()
 
+
 # formulario para registro de espacos fisicos do evento
-def formulario_espaco_fisico( evento, request ):
-    form_espaco_fisico = RegistrarEspacoFisicoEventoForm( request.POST )
+def formulario_espaco_fisico(evento, request):
+    form_espaco_fisico = RegistrarEspacoFisicoEventoForm(request.POST)
 
     if form_espaco_fisico.is_valid():
-        espaco_fisico = form_espaco_fisico.save( commit=False )
+        espaco_fisico = form_espaco_fisico.save(commit=False)
         espaco_fisico.evento = evento
         espaco_fisico.save()
+
 
 # # formulario para registro de periodo do evento
 # def formulario_periodo( form_trilha_atividade, request ):
@@ -130,32 +141,29 @@ def formulario_espaco_fisico( evento, request ):
 #         periodo = form_periodo.save( commit=False )
 #         periodo.save()
 
-#TODO
-def formulario_trilhe_evento( request ):
-    form_trilha_atividade = TrilhaAtividadeEventoForm( request.POST )
+# TODO
+def formulario_trilhe_evento(request):
+    form_trilha_atividade = TrilhaAtividadeEventoForm(request.POST)
+
 
 # formulario para registrar um evento
-def formulario_registrar_evento( form_periodo, form_endereco, form_add_evento, self ):
-
+def formulario_registrar_evento(form_periodo, form_endereco, form_add_evento, self):
     if form_periodo.is_valid() and form_endereco.is_valid() and form_add_evento.is_valid():
-        endereco = form_endereco.save( commit=False )
+        endereco = form_endereco.save(commit=False)
         # metodo para consultar o cep
-
-        adress = pycep_correios.consultar_cep( endereco.cep )
-        print( adress )
-        endereco.cidade = adress[ 'cidade' ]
-        endereco.estado = adress[ 'uf' ]
-        endereco.logradouro = adress[ 'end' ]
-        endereco.bairro = adress[ 'bairro' ]
+        adress = pycep_correios.consultar_cep(endereco.cep)
+        endereco.cidade = adress['cidade']
+        endereco.estado = adress['uf']
+        endereco.logradouro = adress['end']
+        endereco.bairro = adress['bairro']
+        endereco.pais = "Brasil"
+        endereco.numero = "3130"
         endereco.save()
-
-        periodo = form_periodo.save( commit=False )
+        periodo = form_periodo.save(commit=False)
         periodo.save()
-
         # pega os dados preenchidos no formulario na opção de tipo evento
-        tipo_evento = self.request.POST[ 'tipo_evento' ]
-
-        evento = form_add_evento.save( commit=False )
+        tipo_evento = self.request.POST['tipo_evento']
+        evento = form_add_evento.save(commit=False)
         evento.dono = self.request.user
         evento.tipo_evento = tipo_evento
         evento.periodo = periodo
@@ -163,7 +171,3 @@ def formulario_registrar_evento( form_periodo, form_endereco, form_add_evento, s
 
         # tag_evento.save()
         evento.save()
-
-        # except CEPInvalido as exc:
-        #     print( exc )
-
