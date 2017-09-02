bf151a8535d89b845ffa594acda37e7ff5584f9a
KassioHolanda <Leodido98006627>
2017-09-02 10:22:14 -0300

    corrigindo conflitos com master

--- user/models.py
+++ user/models.py
@@ -19,7 +19,7 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
         validators=[validators.RegexValidator(re.compile('^[\w.@+-]+$'),
                                               'O nome do user so pode conter letras, digitos ou os''seguintes caracteres @/./+/-/_'
                                               'invalid')])
-   
+
     email = models.EmailField('E-mail', unique=True)
     nome = models.CharField('Nome', max_length=100, blank=False)
     data_de_entrada = models.DateTimeField('Data de entrada', auto_now_add=True)
@@ -29,7 +29,6 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     tags = models.ManyToManyField('core.Tag',
                                   through="core.Tag_Usuario",
                                   related_name='tags_do_usuario')
-
 
     class Meta:
         verbose_name = 'Usuário'
@@ -89,7 +88,6 @@ class Inscricao(models.Model):
             item_inscricao.atividade = atividade
             item_inscricao.save()
 
-
     def validate_periodo_inscricao(self):
         if self.evento.status != StatusEvento.INSCRICOES_ABERTAS:
             return ValidationError("Periodo de inscricoes ja encerrou")
@@ -103,7 +101,6 @@ class Inscricao(models.Model):
         super(Inscricao, self).save()
 
 
-
 class CheckinItemInscricao(models.Model):
     data = models.DateField('Data de entrada', auto_now_add=True)
     hora = models.TimeField("Hora", blank=True, null=False, default="00:00")
@@ -113,6 +110,20 @@ class CheckinItemInscricao(models.Model):
     gerente = models.ForeignKey("user.Usuario",
                                 related_name="gerente_chekin",
                                 default="")
+
+    def validate_gerente_chekin(self):
+        if self.status == StatusCheckIn.VERIFICADO and self.gerente == "":
+            raise ValidationError('Gerente nao Informado')
+        if self.status == StatusCheckIn.NAO_VERIFICADO and self.gerente != "":
+            raise ValidationError('Status do Checkin nao foi alterado')
+
+    def clean(self):
+        super(CheckinItemInscricao, super).clean()
+        self.validate_gerente_chekin()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(CheckinItemInscricao, self).save()
 
 
 class ItemInscricao(models.Model):
@@ -124,4 +135,23 @@ class ItemInscricao(models.Model):
                                   blank=True, default="")
 
     checkin = models.ForeignKey('CheckinItemInscricao',
-                                default="" , null=True)
+                                default="", null=True)
+
+    def validate_atividade_existente(self):
+        for atividade in self.inscricao.atividade.atividades:
+            if atividade == self.atividade:
+                raise ValidationError('Voce ja se inscreveu nessa atividade')
+
+    def validate_atividade_evento(self):
+        pass
+
+    def validate_conflito_horario_atividade(self):
+        pass
+
+    def clean(self):
+        super(ItemInscricao, self).clean()
+        self.validate_atividade_existente()
+
+    def save(self):
+        self.full_clean()
+        super(ItemInscricao, self).save()
