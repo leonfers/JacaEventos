bb7e2fe80c08d3361b5249c548503ac31143c4d5
KassioHolanda <holandakassio@hotmail.com>
2017-09-04 07:17:43 -0300

    organizando o pacote core

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from core.views import RegistrarEvento, RegistrarInstituicoes, MeusEventos, Part
 
 urlpatterns = [
     url('^eventos/$', MeusEventos.as_view(), name='meus_eventos'),
-    url('^eventos/(?P<eventos_id>\d+)$', ExibirDetalhesEvento.as_view(), name='exibir_detalhes_evento'),
+    url('^eventos/exibir_evento=(?P<eventos_id>\d+)$', ExibirDetalhesEvento.as_view(), name='exibir_detalhes_evento'),
     url('^registrar_evento/$', RegistrarEvento.as_view(), name='registrar_eventos'),
     url('^registrar_instituicoes/$', RegistrarInstituicoes.as_view(), name='registrar_instituicoes'),
     url('^participar_evento/$', ParticiparEvento.as_view(), name='participar_evento'),
